afaab3dbe5e57d4c4a6f389ac074ecea8700d862
MorpheusFR <Creatorsk@gmail.com>
2018-08-17 17:06:09 +0300

    Fist start

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root to: 'home#index'
+
+  # devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  resources :users
+  resources :authors
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
